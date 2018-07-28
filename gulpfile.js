3c680e2e867cdf52e9b80fd83167039434127db1
Rodrigo Koga <rodrigo.koga@dataprev.gov.br>
2018-07-28 19:42:41 -0300

    :poop:

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,15 @@ let uglify = require("gulp-uglify");
 gulp.task('scripts',()=>{
 	return gulp.src("scripts/**")
 	.pipe(concat('scripts.js'))
-	.pipe(gulp.dest("build/"))
+	.pipe(gulp.dest("public/build/"))
 })
 
 gulp.task('browserify',()=>{
-	return browserify("build/scripts.js")
+	return browserify("public/build/scripts.js")
 	.transform("babelify",{presets:["@babel/preset-env"]})
 	.bundle()
-	.pipe(fs.createWriteStream("build/bundle.js"))
+	.pipe(fs.createWriteStream("public/build/bundle.js"))
 })
 
 gulp.task('default',['scripts','browserify']);
-gulp.watch("scripts/**",['scripts','browserify'])
+gulp.watch("scripts/**",['scripts','browserify']);
--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,18 @@ import $ from 'jquery';
 $(document).ready(()=>{
   let scene = $("#scene")[0];
   let parallaxInstance = new Parallax(scene);
+
+  let $kid = $(".kid");
+  let $message = $(".message");
+
+  $(".island").click((e)=>{
+    let $island = $(e.currentTarget);
+    $island.toggleClass("island-big");
+    $kid.toggleClass("kid-big");
+  });
+
+
+  $kid.click((e)=>{
+    $message.css("visibility","visible");
+  })
 });
